4c6f2f98c2df48363074029c87ddbbc115d2377e
laurencechengithub <laurence.email@gmail.com>
2022-06-15 17:21:35 +0800

    [create]admin.py and test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin # noqa 
+from django.contrib import admin # noqa
 # noqa tells the flake8 to not lookinto that line of code
 # Register your models here.
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin page"""
+    ordering = ["id"]
+    #ordering comes with the django useradmin
+    list_display = ["email","name"]
+
+admin.site.register(models.User, UserAdmin)
+#if without UserAdmin will basic CRUD operation in models.user
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+
+    #the test name for admin are not the same as from model starting as "test"
+    def setUp(self):
+        """Create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="adminTest@example.com"
+            password="adminTest123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="userTest@example.com"
+            password="userTest123"
+            name="Test user"
+        )
+
+    def test_users_list(self):
+        """test that users are on the page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
