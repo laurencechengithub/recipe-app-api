baac5d7d6133bd2745df2986f883b1bfa9f64f60
laurencechengithub <laurence.email@gmail.com>
2022-06-22 18:09:47 +0800

    [create] override the django create recipe function for + tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -23,6 +23,26 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id','title','time_minutes','price','link','tags']
         read_only_fields = ['id']
 
+    #default nested serializer are read only, thus we need to create custom
+    #adding a new method in class to override the original create method
+    def create(self, validated_data):
+        "create a recipe"
+        #tags exists, remove it from validated_data and asign it to new varibale
+        tags = validated_data.pop('tags',[])
+        #and the rest of validated_data (that tags are exculded)
+        #we are going to create recipe with those value
+        recipe = Recipe.objects().create(**validated_data)
+        #get the authenticated user
+        auth_user = self.context["request"].user
+        for tag in tags:
+            tag_obj, created = Tag.objects().get_or_create(
+                user=auth_user,
+                **tag, # name=tag['name'],
+
+            )
+            recipe.tags.add(tag_obj)
+        return recipe
+
 
 # using RecipeSerializer as the base class since it's just an extension of the that
 # with some few extra fields only
