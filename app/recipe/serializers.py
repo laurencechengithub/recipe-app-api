ae4a8f25f96ba1093a659d0cc255ac693756e555
laurencechengithub <laurence.email@gmail.com>
2022-06-27 11:22:00 +0800

    [fix] add image url into recipes detail api response

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -98,7 +98,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 # with some few extra fields only
 class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description','image']
 
 
 class RecipeImageSerializer(serializers.ModelSerializer):
