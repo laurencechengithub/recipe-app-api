f35aa8eb8a06bd24c77f34a2754a7e5e363d73cf
laurencechengithub <laurence.email@gmail.com>
2022-06-27 14:18:59 +0800

    [create] add code for filtering

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -23,6 +23,16 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+
+#filter
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+
+
 # ModelViewSet is use the direct interact with a model
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe api"""
@@ -33,11 +43,30 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    #recieving filter arguments as a list of ids, comma seperated
+    def _params_to_ints(self, qs):
+        'convert a list of string to intergers'
+        return [int(str_id) for str_id in qs.split(',')]
+
+
     # override the orignal function to filter the queryset thats related to the user
     def get_queryset(self):
         """Retrieve recipes for authenticated user"""
         #user that is assigned with the request
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-id').distinct()
+
 
     def get_serializer_class(self):
         "Return the serializer class for the request"
