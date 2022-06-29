500992fd81e64122f0d7a995bd6c1d2c12fdff67
laurencechengithub <laurence.email@gmail.com>
2022-06-29 14:17:55 +0800

    [create] server health-check api

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,10 +23,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #generate the yaml file of this project's api
+    path('api/health-check', core_views.health_check, name='health_check'),
     path(
         'api/schema/',
         SpectacularAPIView.as_view(),
